93d2fd3d42c4ed55237695e7fd665db8bc05764c
hxvac <br4nddonontiktok@gmail.com>
2026-03-11 17:35:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,185 @@ let players = [
 
 {num:15,name:"Quinn Poland",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
 
-{num:21,name:"Regan Oplotnik",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0}
+{num:21,name:"Regan Oplotnik",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:22,name:"Karsyn O'Rand",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:23,name:"Erika Boatman",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:24,name:"August Mooneyham",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:25,name:"Khloe Harryman",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:41,name:"Callie Trezona",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:52,name:"Hemi Oney",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0}
 
 ];
+
+function render(){
+
+let table="";
+let select="";
+
+players.forEach((p,i)=>{
+
+table += `
+<tr>
+<td>${p.num}</td>
+<td>${p.name}</td>
+<td>${p.pts}</td>
+<td>${p.fgm}-${p.fga}</td>
+<td>${p.threeM}-${p.threeA}</td>
+<td>${p.ftm}-${p.fta}</td>
+<td>${p.reb}</td>
+<td>${p.ast}</td>
+<td>${p.pf}</td>
+<td>${p.to}</td>
+</tr>
+`;
+
+select += `<option value="${i}">#${p.num} ${p.name}</option>`;
+
+});
+
+document.getElementById("playerRows").innerHTML=table;
+document.getElementById("playerSelect").innerHTML=select;
+
+updateTotals();
+
+}
+
+function selected(){
+return players[document.getElementById("playerSelect").value];
+}
+
+function add2(){
+
+let p=selected();
+
+p.pts+=2;
+p.fgm+=1;
+p.fga+=1;
+
+render();
+
+}
+
+function add3(){
+
+let p=selected();
+
+p.pts+=3;
+p.fgm+=1;
+p.fga+=1;
+p.threeM+=1;
+p.threeA+=1;
+
+render();
+
+}
+
+function fgMiss(){
+
+selected().fga+=1;
+
+render();
+
+}
+
+function threeMiss(){
+
+selected().threeA+=1;
+selected().fga+=1;
+
+render();
+
+}
+
+function ftMake(){
+
+let p=selected();
+
+p.pts+=1;
+p.ftm+=1;
+p.fta+=1;
+
+render();
+
+}
+
+function ftMiss(){
+
+selected().fta+=1;
+
+render();
+
+}
+
+function reb(){
+
+selected().reb+=1;
+
+render();
+
+}
+
+function ast(){
+
+selected().ast+=1;
+
+render();
+
+}
+
+function pf(){
+
+selected().pf+=1;
+
+render();
+
+}
+
+function turnover(){
+
+selected().to+=1;
+
+render();
+
+}
+
+function updateTotals(){
+
+let totals={
+pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0
+};
+
+players.forEach(p=>{
+
+totals.pts+=p.pts;
+totals.fgm+=p.fgm;
+totals.fga+=p.fga;
+totals.threeM+=p.threeM;
+totals.threeA+=p.threeA;
+totals.ftm+=p.ftm;
+totals.fta+=p.fta;
+totals.reb+=p.reb;
+totals.ast+=p.ast;
+totals.pf+=p.pf;
+totals.to+=p.to;
+
+});
+
+document.getElementById("tPTS").innerText=totals.pts;
+document.getElementById("tFG").innerText=`${totals.fgm}-${totals.fga}`;
+document.getElementById("t3FG").innerText=`${totals.threeM}-${totals.threeA}`;
+document.getElementById("tFT").innerText=`${totals.ftm}-${totals.fta}`;
+document.getElementById("tREB").innerText=totals.reb;
+document.getElementById("tAST").innerText=totals.ast;
+document.getElementById("tPF").innerText=totals.pf;
+document.getElementById("tTO").innerText=totals.to;
+
+}
+
+render();
