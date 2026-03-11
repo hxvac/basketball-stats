cf23d0ea929a53a9d0b98d1300f99fdbf623a9ea
hxvac <br4nddonontiktok@gmail.com>
2026-03-11 17:32:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let players = [
 
+{num:1,name:"Madelyn Scheibe",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:2,name:"Brilee Black",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:3,name:"Avery Brooks",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:5,name:"Macy Mahnken",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:10,name:"Marli Sisney",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:12,name:"Kennedy Keller",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:13,name:"Emma Bowman",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:14,name:"Maddie Coester",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:15,name:"Quinn Poland",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0},
+
+{num:21,name:"Regan Oplotnik",pts:0,fgm:0,fga:0,threeM:0,threeA:0,ftm:0,fta:0,reb:0,ast:0,pf:0,to:0}
+
+];
